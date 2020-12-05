b532d82344e870bed02d79667774af3fce827162
kimseunghun <rlatmdgns3076@gmail.com>
2020-12-05 21:45:04 +0900

    로아웃그

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import Login from './components/login/login';
 import styles from './app.module.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Maker from './components/maker/maker';
 function App({authService}) {
   return (
-    <Login className={styles.app} authService={authService}/>
+    <div className={styles.app} >
+      <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Login authService={authService}/>
+        </Route>
+        <Route path="/maker">
+          <Maker authService={authService}/>
+        </Route>
+      </Switch>
+
+      </BrowserRouter>
+    </div>
   );
 }
 
