ead7c2e182f026624be145cb6605952275343cf0
kimseunghun <rlatmdgns3076@gmail.com>
2021-01-05 02:08:39 +0900

    handle state change editor

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -2,13 +2,20 @@ import React from "react";
 import CardAddForm from "../card_add_form/card_add_from";
 import CardEditForm from "../card_edit_form/card_edit_from";
 import styles from "./editor.module.css";
-const Editor = ({ cards, addCard }) => {
+const Editor = ({ cards, addCard, updateCard, deleteCard }) => {
   console.log(cards);
   return (
     <section className={styles.editor}>
       <h1 className={styles.title}>Card Maker</h1>
-      {cards.map((card) => {
-        return <CardEditForm key={card.id} card={card} />;
+      {Object.keys(cards).map((key) => {
+        return (
+          <CardEditForm
+            key={key}
+            card={cards[key]}
+            updateCard={updateCard}
+            deleteCard={deleteCard}
+          />
+        );
       })}
       <CardAddForm onAdd={addCard} />
     </section>
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -6,8 +6,8 @@ import { useHistory } from "react-router-dom";
 import Editor from "../editor/editor";
 import Preview from "../preview/preview";
 const Maker = ({ authService }) => {
-  const [cards, setCards] = useState([
-    {
+  const [cards, setCards] = useState({
+    1: {
       id: "1",
       name: "승훈",
       company: "flow",
@@ -18,7 +18,7 @@ const Maker = ({ authService }) => {
       fileName: "tmdgns",
       fileURL: null,
     },
-    {
+    2: {
       id: "2",
       name: "승훈",
       company: "flow",
@@ -29,7 +29,7 @@ const Maker = ({ authService }) => {
       fileName: "tmdgns",
       fileURL: "tmdgns.png",
     },
-    {
+    3: {
       id: "3",
       name: "승훈",
       company: "flow",
@@ -40,7 +40,7 @@ const Maker = ({ authService }) => {
       fileName: "tmdgns",
       fileURL: null,
     },
-  ]);
+  });
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -53,16 +53,43 @@ const Maker = ({ authService }) => {
     });
   });
 
-  const addCard = (card) => {
-    const updated = [...cards, card];
-    setCards(updated);
+  // const addCard = (card) => {
+  //   const updated = [...cards, card];
+  //   setCards(updated);
+  // };
+  // const updateCard = (card) => {
+  //   const updated = { ...cards };
+  //   updated[card.id] = card;
+  //   setCards(updated);
+  // };
+  const createOrUpdateCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      updated[card.id] = card;
+      return updated;
+    });
+    //콜백 함수 전달
+    //이전에 cards를 받아서 리턴
+    // state 가 오래 된거 이거나 , 동기적으로 할 수 없을 때
+  };
+  const deleteCard = (card) => {
+    setCards((cards) => {
+      const updated = { ...cards };
+      delete updated[card.id];
+      return updated;
+    });
   };
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} addCard={addCard} />
-        <Preview cards={cards} addCard={addCard} />
+        <Editor
+          cards={cards}
+          addCard={createOrUpdateCard}
+          updateCard={createOrUpdateCard}
+          deleteCard={deleteCard}
+        />
+        <Preview cards={cards} />
       </div>
       <Footer />
     </section>
